562145f293cb50551f60e3d3543994b833559e8b
Clement Marescaux <cmarescaux@hotmail.com>
2024-07-31 23:38:53 +0200

    Fix unused fields lint

--- bg3_lib/src/lspk_header.rs
+++ bg3_lib/src/lspk_header.rs
@@ -4,9 +4,9 @@ use serde::Deserialize;
 pub struct LSPKHeader16 {
     pub version: u32,
     pub file_list_offset: u64,
-    pub file_list_size: u32,
+    pub _file_list_size: u32,
     pub flags: u8,
     pub priority: u8,
-    pub md5: [u8; 16],
-    pub num_parts: u16,
+    pub _md5: [u8; 16],
+    pub _num_parts: u16,
 }
